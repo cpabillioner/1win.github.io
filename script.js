7d0a7fe5c93ad5e9f554d5b6114ed4fce0aad396
Ivan Sidorenko <ivansed1997@gmail.com>
2023-10-12 10:53:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const iframe = document.getElementById('main-iframe');
 const loadingSpinner = document.getElementById('loading-spinner');
+const logos = document.
 
 // Hide the loading spinner when the iframe is fully loaded
 iframe.addEventListener('load', () => {
     loadingSpinner.classList.add('hidden');
+    document.querySelectorAll('.install').forEach(install => install.style.display = 'block')
 });
 
 // Show the loading spinner initially
