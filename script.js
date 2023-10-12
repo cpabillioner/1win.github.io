618e1a3e2dbd9776499959e1429c470572ac3a81
Ivan Sidorenko <ivansed1997@gmail.com>
2023-10-12 10:58:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const iframe = document.getElementById('main-iframe');
 const btnContainer = document.getElementById('button-container');
 const loadingSpinner = document.getElementById('loading-spinner');
-const logos = document.
 
 // Hide the loading spinner when the iframe is fully loaded
 iframe.addEventListener('load', () => {
