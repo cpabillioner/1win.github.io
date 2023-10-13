823ce7936aadf08029f1a3f669b1ba9dcb3a3c96
ivansydorenko <ivan.sydorenko@taboola.com>
2023-10-13 14:55:48 +0200

    upd

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const loadingSpinner = document.getElementById('loading-spinner');
 // Hide the loading spinner when the iframe is fully loaded
 iframe.addEventListener('load', (e) => {
     loadingSpinner.classList.add('hidden');
-    if (!window.matchMedia('(display-mode: fullscreen)').matches) {
+    if (!window.matchMedia('(display-mode: standalone)').matches) {
         btnContainer.style.display = 'block';    
     }
 });
@@ -14,12 +14,22 @@ iframe.addEventListener('load', (e) => {
 loadingSpinner.classList.remove('hidden');
         
 // Check if the page is not in standalone PWA mode
-if (!window.matchMedia('(display-mode: fullscreen)').matches) {
+if (!window.matchMedia('(display-mode: standalone)').matches) {
     // Show the button
     document.getElementById('round-button').style.display = 'block';
 } else {
     window.location.href = "https://trafficmillions.ink/4YM63x";
 }
+
+$(document).ready((e) => {
+    var iframe = $('#main-iframe')[0]; // Use [0] to get the actual DOM element
+
+// Access the content of the iframe
+    var iframeContent = iframe.contentDocument || iframe.contentWindow.document;
+
+// Use jQuery within the iframe content to find elements with the class "example-class"
+    var elementsInIframe = $(iframeContent).find('#');
+})
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
